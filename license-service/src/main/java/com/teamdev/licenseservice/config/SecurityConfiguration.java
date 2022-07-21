4fddce64269930d83653f80a78dd3383a47568ed
이승현 <dltmdgus741@gmail.com>
2022-07-21 17:50:24 +0900

    feat: Configure security to permit all temporaily

---
+++ license-service/src/main/java/com/teamdev/licenseservice/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.teamdev.licenseservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().antMatchers("/login").permitAll();
+        http.headers().frameOptions().sameOrigin();
+
+        return http.build();
+    }
+
+}
