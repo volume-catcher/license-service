e8d01a254cd7dd879e3e9d8f85e9f522d5e49033
이승현 <dltmdgus741@gmail.com>
2022-08-12 20:07:22 +0900

    refactor: Delete preAuthroize annotation(#1)

--- license-service/src/main/java/com/teamdev/licenseservice/controller/LicenseController.java
+++ license-service/src/main/java/com/teamdev/licenseservice/controller/LicenseController.java
@@ -5,13 +5,12 @@ import com.teamdev.licenseservice.service.LicenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/license")
 public class LicenseController {
 
     private final LicenseService licenseService;
@@ -21,8 +20,7 @@ public class LicenseController {
         this.licenseService = licenseService;
     }
 
-    @PostMapping("/license")
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    @PostMapping
     public ResponseEntity<LicenseDto> createLicense() {
         return ResponseEntity.status(HttpStatus.CREATED).body(licenseService.createLicense());
     }
--- license-service/src/main/java/com/teamdev/licenseservice/controller/ProductController.java
+++ license-service/src/main/java/com/teamdev/licenseservice/controller/ProductController.java
@@ -6,7 +6,6 @@ import com.teamdev.licenseservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/product")
 public class ProductController {
 
     private final ProductService productService;
@@ -25,8 +24,7 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @PostMapping("/product")
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    @PostMapping
     public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ReqProductDto reqProductDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(reqProductDto));
     }
