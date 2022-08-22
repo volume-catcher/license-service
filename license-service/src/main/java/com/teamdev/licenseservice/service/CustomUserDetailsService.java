15ae84da31dd71450f71fe40e495e9168fe73047
이승현 <dltmdgus741@gmail.com>
2022-08-23 04:40:21 +0900

    refactor: 불필요한 코드 제거(#4)

--- license-service/src/main/java/com/teamdev/licenseservice/service/CustomUserDetailsService.java
+++ license-service/src/main/java/com/teamdev/licenseservice/service/CustomUserDetailsService.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,16 +21,18 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final AccountRepository accountRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) {
         return accountRepository.findOneWithRolesById(username)
-                .map(user -> createUser(username, user))
+                .map(this::createUser)
                 .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
     }
 
-    private org.springframework.security.core.userdetails.User createUser(String username, Account account) {
+    private org.springframework.security.core.userdetails.User createUser(Account account) {
         List<GrantedAuthority> grantedRoles = account.getRoles().stream()
                 .map(role -> new SimpleGrantedAuthority(role.getName()))
                 .collect(Collectors.toList());
+
         return new org.springframework.security.core.userdetails.User(account.getId(),
                 account.getPassword(),
                 grantedRoles);
