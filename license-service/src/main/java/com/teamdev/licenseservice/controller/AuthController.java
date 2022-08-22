60f10f051648cdd4102dd2b4ddb892cec5d6f578
이승현 <dltmdgus741@gmail.com>
2022-08-23 04:12:13 +0900

    refactor: controller의 signin 로직 service로 위임, 토큰 바디에 전달(#4)

--- license-service/src/main/java/com/teamdev/licenseservice/controller/AuthController.java
+++ license-service/src/main/java/com/teamdev/licenseservice/controller/AuthController.java
@@ -1,16 +1,9 @@
 package com.teamdev.licenseservice.controller;
 
 import com.teamdev.licenseservice.dto.SignInDto;
-import com.teamdev.licenseservice.jwt.TokenProvider;
+import com.teamdev.licenseservice.dto.TokenDto;
+import com.teamdev.licenseservice.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,26 +16,11 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final TokenProvider tokenProvider;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
-    @Value("${jwt.header}")
-    public String authorizationHeader;
-    @Value("${jwt.bearer}")
-    public String authorizationBearer;
+    private final AuthService authService;
 
     @PostMapping("/signin")
-    public ResponseEntity signIn(@Valid @RequestBody SignInDto signInDto) {
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(signInDto.getId(), signInDto.getPassword());
-
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String jwt = tokenProvider.createToken(authentication);
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(authorizationHeader, authorizationBearer + " " + jwt);
-
-        return new ResponseEntity<>(httpHeaders, HttpStatus.OK);
+    public TokenDto signIn(@Valid @RequestBody SignInDto signInDto) {
+        return authService.signIn(signInDto);
     }
 
 }
---
+++ license-service/src/main/java/com/teamdev/licenseservice/service/AuthService.java
@@ -0,0 +1,30 @@
+package com.teamdev.licenseservice.service;
+
+import com.teamdev.licenseservice.dto.SignInDto;
+import com.teamdev.licenseservice.dto.TokenDto;
+import com.teamdev.licenseservice.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    public TokenDto signIn(SignInDto signInDto) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(signInDto.getId(), signInDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        String jwt = tokenProvider.createToken(authentication);
+
+        return new TokenDto(jwt);
+    }
+
+}
